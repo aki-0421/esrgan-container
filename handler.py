ecb5c764bafec2d3d50c198096a4f7af5474c7d7
aki-0421 <118268728+aki-0421@users.noreply.github.com>
2023-04-03 16:42:07 +0900

    R: fix

--- handler.py
+++ handler.py
@@ -48,14 +48,17 @@ def handler(job):
     image_file_path = rp_download.download_input_objects(
         [validated_input.get('image_url', None)]
     )  # pylint: disable=unbalanced-tuple-unpacking
+    image = image_file_path[0]
 
-    imgname, extension = os.path.splitext(os.path.basename(image_file_path))
-    img = cv2.imread(image_file_path, cv2.IMREAD_UNCHANGED)
+    imgname, extension = os.path.splitext(os.path.basename(image))
+    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
     if len(img.shape) == 3 and img.shape[2] == 4:
         img_mode = 'RGBA'
     else:
         img_mode = None
 
+
+    image_url = ""
     try:
         output, _ = upsampler.enhance(img, outscale=outscale)
     except RuntimeError as error:
@@ -69,7 +72,9 @@ def handler(job):
 
         cv2.imwrite(save_path, output)
 
-    return save_path
+        image_url = rp_upload.upload_image(job['id'], save_path)
+
+    return image_url
 
 
 runpod.serverless.start({
